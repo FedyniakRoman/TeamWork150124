9b4fdcf1dfa30d7656c2c29608dcc873db86fdab
EmtecAmic <romakrasov@gmail.com>
2024-08-15 13:59:04 +0200

    task

---
+++ RomanFedyniak.js/durations.js
@@ -0,0 +1,34 @@
+db.tracks.aggregate([
+  {
+    $match: {
+      duration_secs: { $lt: 60000 },
+    },
+  },
+
+  {
+    $sort: {
+      duration_secs: 1,
+    },
+  },
+
+  {
+    $limit: 3,
+  },
+
+  {
+    $lookup: {
+      from: "users",
+      localField: "author_id",
+      foreignField: "_id",
+      as: "author",
+    },
+  },
+
+  {
+    $project: {
+      title: 1, 
+      duration_secs: 1,
+
+    },
+  },
+]);
