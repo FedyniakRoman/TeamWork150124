9e945e817a9cc2a40728ed844b1c4dc29eb88419
alona <aloyna.vasilieva@gmail.com>
2024-08-15 14:50:25 +0300

    added task_4

---
+++ Vasylieva/task_4.js
@@ -0,0 +1,30 @@
+db.tracks.aggregate([
+  {
+    $match: {
+      duration_seconds: { $lt: 60000 },
+    }
+  },
+  {
+    $sample: { size: 3 }
+  },
+  {
+    $sort: { duration_seconds: 1 }
+  },
+  {
+    $lookup: {
+      from: "authors",
+      localField: "author_id",
+      foreignField: "_id",
+      as: "author",
+    }
+  },
+
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+      duration_secs: 1,
+      author_name: "$author.name",
+    }
+  }
+]);
