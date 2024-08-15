cdad44ceab9f8b1f578e82128a2b5ccca8f016b4
Beljawski <155641687+Beljawski@users.noreply.github.com>
2024-08-15 13:53:04 +0200

    first commit

---
+++ MariaBeljawski/task2_1508.js
@@ -0,0 +1,32 @@
+// **Задача. Вывести данные о трех случайных треках продолжительностью до `1000 мин` (не вкл.) в порядке увеличения продолжительности**
+
+// проекция: `название_трека`, `продолжительность_трека`, `имя_автора`
+
+db.tracks.aggregate([
+  {
+    $lookup: {
+      from: "users",
+      localField: "author_id",
+      foreignField: "_id",
+      as: "user_info",
+    },
+  },
+  {
+    $match: {
+      duration_secs: { $lt: 1000 * 60 },
+    },
+  },
+  {
+    $sample: { size: 3 },
+  },
+  {
+    $sort: { duration_secs: 1 },
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+      duration_secs: 1,
+    },
+  },
+]);
