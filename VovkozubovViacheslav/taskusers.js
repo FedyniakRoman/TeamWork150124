f3c94efd80e892f79a1705e9686b66b647ea354d
Slava <slavok.v005@mail.ru>
2024-08-20 12:33:31 +0200

    new

---
+++ VovkozubovViacheslav/taskusers.js
@@ -0,0 +1,10 @@
+// Задача. Вывести общее ко-во клиентов из France (предложить несколько вариантов)
+db.users.aggregate([
+    { $match: { country: 'France' } },
+    {
+        $group: {
+            _id: null,
+            total: { $count: {} }
+        }
+    }
+])
