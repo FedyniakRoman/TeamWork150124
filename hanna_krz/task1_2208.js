929a786b820d32b0703f2492554979da9a8dab71
Hanna Krzh <anutkakrz@gmail.com>
2024-08-22 13:12:12 +0200

    task1_2208.js

---
+++ hanna_krz/task1_2208.js
@@ -0,0 +1,9 @@
+db.users.aggregate([
+    { $match: { is_blocked: { $ne: true } } },
+    { $sample: { size: 1 } },
+    {
+        $project: {
+            fullname: 1,
+            _id: 0
+        }}
+])
