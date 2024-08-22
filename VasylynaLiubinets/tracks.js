c68296781b654951a3f643864d21bdbb469cc1e7
Vasylyna <alina8989_08@ukr.net>
2024-08-22 14:00:29 +0200

    tracks

---
+++ VasylynaLiubinets/tracks.js
@@ -0,0 +1,12 @@
+db.tracks.aggregate([
+    {
+        $match: { duration_secs: { $gte: 5 * 60 } }
+    },
+
+    {
+        $group: {
+            _id: null,
+            avg_duration: { $avg: "$duration_secs" }
+        }
+    }
+])
